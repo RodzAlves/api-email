766bd50e16325926cfedfe23e8b1ea8dcf033b74
RodzAlves <rodrigoalvesbrasileiro@gmail.com>
2021-03-08 21:15:29 -0300

    feat: Create User method in controller.

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,11 +1,37 @@
 import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import { User } from '../models/User';
 
 class UserController {
   async create(request: Request, response: Response) {
-    const data = request.body;
-    console.log(data);
+    try {
+      const { name, email } = request.body;
 
-    return response.send();
+      const usersRepository = getRepository(User);
+
+      const checkUserExists = await usersRepository.findOne({
+        where: { email },
+      });
+
+      if (checkUserExists) {
+        return response.status(400).json({
+          error: 'Email address already used.',
+        });
+      }
+
+      const user = usersRepository.create({
+        name,
+        email,
+      });
+
+      await usersRepository.save(user);
+
+      return response.json(user);
+    } catch (err) {
+      return response.status(400).json({
+        error: err.message,
+      });
+    }
   }
 }
 
